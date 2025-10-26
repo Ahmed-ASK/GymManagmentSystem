e2b952c3e1ea4218a40ca6295f3e7d9c17257185
Ahmed adel <yarab8308@gmail.com>
2025-10-26 15:14:06 +0300

    Ambigous between Core and non-Core Packages

--- GymManagementDAL/Data/DataSeed/IdentityDataSeeding.cs
+++ GymManagementDAL/Data/DataSeed/IdentityDataSeeding.cs
@@ -1,70 +1,71 @@
 ﻿using GymManagementDAL.Entities;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace GymManagementDAL.Data.DataSeed
 {
-	public static class IdentityDataSeeding
-	{
-		public static bool SeedData(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
-		{
-			try
-			{
-				bool HasUsers = userManager.Users.Any();
-				bool HasRoles = roleManager.Roles.Any();
+    public static class IdentityDataSeeding
+    {
+        public static bool SeedData(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            try
+            {
+                bool HasUsers = userManager.Users.Any();
+                bool HasRoles = roleManager.Roles.Any();
 
-				if (HasUsers && HasRoles) return false;
+                if (HasUsers && HasRoles) return false;
 
-				if (!HasRoles)
-				{
-					var Roles = new List<IdentityRole>()
-					{
-						new IdentityRole(){Name = "SuperAdmin"},
-						new IdentityRole(){Name = "Admin"}
-					};
+                if (!HasRoles)
+                {
+                    var Roles = new List<IdentityRole>()
+                    {
+                        new IdentityRole() { Name = "SuperAdmin" },
+                        new IdentityRole() { Name = "Admin" }
+                    };
 
-					foreach (var Role in Roles)
-					{
-						if (!roleManager.RoleExistsAsync(Role.Name!).Result)
-						{
-							roleManager.CreateAsync(Role).Wait();
-						}
-					}
-				}
-				if (!HasUsers)
-				{
-					var MainAdmin = new ApplicationUser()
-					{
-						FirstName = "Aliaa",
-						LastName = "Tarek",
-						UserName = "AliaaTarek",
-						Email = "Aliaatarek@gmail.com",
-						PhoneNumber = "01123652635"
-					};
+                    foreach (var Role in Roles)
+                    {
+                        if (!roleManager.RoleExistsAsync(Role.Name!).Result)
+                        {
+                            roleManager.CreateAsync(Role).Wait();
+                        }
+                    }
+                }
 
-					userManager.CreateAsync(MainAdmin, "P@ssw0rd").Wait();
-					userManager.AddToRoleAsync(MainAdmin.Id, "SuperAdmin").Wait();
+                if (!HasUsers)
+                {
+                    var MainAdmin = new ApplicationUser()
+                    {
+                        FirstName = "Aliaa",
+                        LastName = "Tarek",
+                        UserName = "AliaaTarek",
+                        Email = "Aliaatarek@gmail.com",
+                        PhoneNumber = "01123652635"
+                    };
+                    userManager.CreateAsync(MainAdmin, "P@ssw0rd").Wait();
+                    // Changed: AddToRoleAsync takes the user object, not ID
+                    userManager.AddToRoleAsync(MainAdmin, "SuperAdmin").Wait();
 
-					var Admin01 = new ApplicationUser()
-					{
-						FirstName = "Omar",
-						LastName = "Mohamed",
-						UserName = "OmarMohamed",
-						Email = "OmarMohamed@gmail.com",
-						PhoneNumber = "01232589652"
-					};
+                    var Admin01 = new ApplicationUser()
+                    {
+                        FirstName = "Omar",
+                        LastName = "Mohamed",
+                        UserName = "OmarMohamed",
+                        Email = "OmarMohamed@gmail.com",
+                        PhoneNumber = "01232589652"
+                    };
+                    userManager.CreateAsync(Admin01, "P@ssw0rd").Wait();
+                    // Changed: AddToRoleAsync takes the user object, not ID
+                    userManager.AddToRoleAsync(Admin01, "Admin").Wait();
+                }
 
-					userManager.CreateAsync(Admin01, "P@ssw0rd").Wait();
-					userManager.AddToRoleAsync(Admin01.Id, "Admin").Wait();
-				}
-				return true;
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"Seeding Failed : {ex}");
-				return false;
-			}
-		}
-
-	}
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Seeding Failed : {ex}");
+                return false;
+            }
+        }
+    }
 }
--- GymManagmentPL/Program.cs
+++ GymManagmentPL/Program.cs
@@ -1,17 +1,20 @@
 using GymManagementBLL;
+using GymManagementBLL.Services.AttachmentService;
 using GymManagementBLL.Services.Classes;
 using GymManagementBLL.Services.Interfaces;
 using GymManagementDAL.Data;
 using GymManagementDAL.Data.DataSeed;
-using GymManagementDAL.Repositiry.Classes;
-using GymManagementDAL.Repositiry.Interfaces;
+using GymManagementDAL.Entities;
 using GymManagementDAL.Repositories.Classes;
 using GymManagementDAL.Repositories.Interfaces;
 using GymManagmentBLL.Services.Classes;
 using GymManagmentBLL.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 
-namespace GymManagmentPL
+namespace GymManagementPL
 {
     public class Program
     {
@@ -20,48 +23,77 @@ namespace GymManagmentPL
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddDbContext<GymDbContext>(options => 
+            builder.Services.AddRazorPages(); // Register Razor Pages
+
+            builder.Services.AddDbContext<GymDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
-            builder.Services.AddAutoMapper(M => M.AddProfile(new MappingProfile()));
-            builder.Services.AddScoped<IMemberService , MemberService>();
-            builder.Services.AddScoped<IPlanRepository, PlanRepository>();
-            builder.Services.AddScoped<ISessionService, SessionService>();
-            builder.Services.AddScoped<ISessionRepository, SessionRepository>();
+
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IMembershipRepository, MembershipRepository>();
+            builder.Services.AddScoped<ISessionRepository, SessionRepository>();
+            builder.Services.AddScoped<IMemberService, MemberService>();
+            builder.Services.AddScoped<ITrainerService, TrainerService>();
+            builder.Services.AddScoped<IPlanService, PlanService>();
+            builder.Services.AddScoped<ISessionService, SessionService>();
+            builder.Services.AddScoped<IMembershipService, MembershipService>();
             builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
-            builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
+            builder.Services.AddScoped<IMemberSessionRepository, MemberSessionRepository>();
+            builder.Services.AddScoped<IBookingService, BookingService>();
+            builder.Services.AddScoped<IAttachmentService, AttachmentService>();
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(config =>
+            {
+                config.Password.RequiredLength = 6;
+                config.Password.RequireLowercase = true;
+            }).AddEntityFrameworkStores<GymDbContext>();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/Login";
+                options.AccessDeniedPath = "/Account/AccessDenied";
+            });
+
+            builder.Services.AddScoped<IAccountService, AccountService>();
+            builder.Services.AddAutoMapper(m => m.AddProfile(new MappingProfile()));
 
             var app = builder.Build();
 
+            #region Migrate Database - Data Seeding
+            using var scope = app.Services.CreateScope();
+            var dbContextObj = scope.ServiceProvider.GetRequiredService<GymDbContext>();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
-            #region Data Seeding
-
-            GymDataSeeding.SeedData(app.Services.CreateScope().ServiceProvider.GetRequiredService<GymDbContext>());
-
+            var pendingMigrations = dbContextObj.Database.GetPendingMigrations();
+            if (pendingMigrations?.Any() ?? false)
+                dbContextObj.Database.Migrate();
+            GymDataSeeding.SeedData(dbContextObj);
+            IdentityDataSeeding.SeedData(roleManager, userManager);
             #endregion
-
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.MapStaticAssets();
-            app.UseHttpsRedirection();
-            app.UseRouting();
 
+            app.UseHttpsRedirection();
+            var provider = new FileExtensionContentTypeProvider();
+            provider.Mappings[".jpeg"] = "image/jpeg";
+            provider.Mappings[".jpg"] = "image/jpeg";
+            provider.Mappings[".png"] = "image/png";
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                ContentTypeProvider = provider
+            });
+            app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
 
-            app.UseStaticFiles();
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
-                .WithStaticAssets();
+            app.MapRazorPages();
 
             app.Run();
         }
